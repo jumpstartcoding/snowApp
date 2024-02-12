d31ee85fbac1df56c9dadd01d701988d231f5677
jumpstartcoding <116022896+jumpstartcoding@users.noreply.github.com>
2024-02-12 17:25:33 -0500

    style fix

--- components/CreateRes.tsx
+++ components/CreateRes.tsx
@@ -4,7 +4,7 @@ import "./SignIn.css";
 export default function CreateRes() {
   return (
     <>
-      <Form className="createRes " style={{ paddingTop: "10px" }}>
+      <Form className="createRes " style={{ padding: "20px" }}>
         <section>
           <Input
             className="input"
