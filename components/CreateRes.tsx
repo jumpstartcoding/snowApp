02a78fce3748d5916fa1d5d0e5fd72709ef7470c
jumpstartcoding <116022896+jumpstartcoding@users.noreply.github.com>
2024-02-13 13:47:45 -0500

    reservation form style fix

--- components/CreateRes.tsx
+++ components/CreateRes.tsx
@@ -4,45 +4,63 @@ import "./SignIn.css";
 export default function CreateRes() {
   return (
     <>
-      <Form className="createRes ">
-        <section>
-          <Input
-            className="input"
-            type="text"
-            name="firstName"
-            id="firstName"
-          />
-          <Input className="input" type="text" name="lastName" id="lastName" />
-        </section>
+      <div
+        style={{
+          marginBottom: "100px",
+        }}
+      >
+        <Form className="card createRes ">
+          <section>
+            <Input
+              className="input"
+              type="text"
+              name="firstName"
+              id="firstName"
+            />
+            <Input
+              className="input"
+              type="text"
+              name="lastName"
+              id="lastName"
+            />
+          </section>
 
-        <section>
-          <Label id="fNameLabel" htmlFor="firstName">
-            First Name
-          </Label>
-          <Label htmlFor="lastName">Last Name</Label>
-        </section>
+          <section>
+            <Label id="fNameLabel" htmlFor="firstName">
+              First Name
+            </Label>
+            <Label htmlFor="lastName">Last Name</Label>
+          </section>
 
-        <Input className="input" type="email" name="email" id="email" />
-        <Label htmlFor="email">Email</Label>
+          <Input className="input" type="email" name="email" id="email" />
+          <Label htmlFor="email">Email</Label>
 
-        <Input className="input" type="tel" name="phone" id="phone" />
-        <Label htmlFor="phone">Phone Number</Label>
+          <Input className="input" type="tel" name="phone" id="phone" />
+          <Label htmlFor="phone">Phone Number</Label>
 
-        <Input className="input" type="number" name="guests" id="guests" />
-        <Label htmlFor="guests"># of Guests</Label>
+          <Input className="input" type="number" name="guests" id="guests" />
+          <Label htmlFor="guests"># of Guests</Label>
 
-        <section style={{ justifyContent: "center" }}>
-          <input type="checkbox" name="skiRes" id="skiRes" />
-          <Label htmlFor="snowBoardRes"> Ski </Label>
-          <input type="checkbox" name="snowBoardRes" id="snowBoardRes" />
+          <section style={{ justifyContent: "center" }}>
+            <input type="checkbox" name="skiRes" id="skiRes" />
+            <Label htmlFor="snowBoardRes"> Ski </Label>
+            <input type="checkbox" name="snowBoardRes" id="snowBoardRes" />
 
-          <Label htmlFor="snowBoardRes"> SnowBoard </Label>
-        </section>
-        <Label htmlFor="date">Date</Label>
+            <Label htmlFor="snowBoardRes"> SnowBoard </Label>
+          </section>
+          <Label htmlFor="date">Date</Label>
 
-        <Input className="input" type="date" id="date" />
-        <Button style={{ position: "relative", top: "1rem" }}>Create</Button>
-      </Form>
+          <Input className="input" type="date" id="date" />
+          <Button
+            // isLoading={true}
+            loadingText="Loading..."
+            type="submit"
+            style={{ position: "relative", top: "1rem" }}
+          >
+            Create
+          </Button>
+        </Form>
+      </div>
     </>
   );
 }
