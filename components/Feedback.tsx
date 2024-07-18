e686a347241471c46edf2bfd6a3980ebba8ffff0
jumpstartcoding <116022896+jumpstartcoding@users.noreply.github.com>
2024-07-18 19:14:38 -0400

    updated -> style changes to feedback page

--- components/Feedback.tsx
+++ components/Feedback.tsx
@@ -2,18 +2,22 @@ import { Button } from "@aws-amplify/ui-react";
 
 export default function Feedback() {
   return (
-    <section className="feedback">
-      <h1>We'd Like Your Feedback</h1>
-      <form action="" method="post">
-        <textarea
-          className="styled-textarea"
-          name="feedback"
-          id="feedback"
-          cols={30}
-          rows={10}
-        ></textarea>
-        <Button type="submit">Submit</Button>
-      </form>
-    </section>
+    <>
+      <section className="feedback">
+        <img className="feedback-img" src="src/assets/feedback.jpg" alt="" />
+        <h1>We'd Like Your Feedback</h1>
+        <form action="" method="post">
+          <label htmlFor="feedbak">Share Your Experience:</label>
+          <textarea
+            className="styled-textarea"
+            name="feedback"
+            id="feedback"
+            cols={30}
+            rows={10}
+          ></textarea>
+          <Button type="submit">Submit</Button>
+        </form>
+      </section>
+    </>
   );
 }
